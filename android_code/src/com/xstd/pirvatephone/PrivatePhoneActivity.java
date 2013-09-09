23037882fa6701f59a7fc87fd87d73afbfc7cbbf
value123 <value_shen@163.com>
2013-09-09 17:09:32 +0800

    123

--- android_code/src/com/xstd/pirvatephone/PrivatePhoneActivity.java
+++ android_code/src/com/xstd/pirvatephone/PrivatePhoneActivity.java
@@ -10,9 +10,16 @@ public class PrivatePhoneActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_private_phone);
+        
+        initView();
     }
 
-    @Override
+    private void initView() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.private_phone, menu);
