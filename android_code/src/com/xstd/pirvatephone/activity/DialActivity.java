d0ad9a5626fc497b198ee2569b457cc67865e3a1
guoqing.sun <guoqing.sun@renren-inc.com>
2013-09-09 15:55:54 +0800

    增加入口布局

---
+++ android_code/src/com/xstd/pirvatephone/activity/DialActivity.java
@@ -0,0 +1,19 @@
+package com.xstd.pirvatephone.activity;
+
+import android.os.Bundle;
+import com.xstd.pirvatephone.R;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: michael
+ * Date: 13-9-9
+ * Time: PM3:11
+ * To change this template use File | Settings | File Templates.
+ */
+public class DialActivity extends BaseActivity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.emergency_dialer);
+    }
+}
