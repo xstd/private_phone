dbb8214d8359e1b1cd03d80c813e40d1f8fe6cbe
guoqing.sun <guoqing.sun@renren-inc.com>
2013-09-12 10:06:42 +0800

    增加DAOdemo

--- android_code/external/PrivatePhoneDaoGenerator/src/com/photo_me/mobile/android/dao/generator/PMDaoGenerator.java
+++ android_code/external/PrivatePhoneDaoGenerator/src/com/photo_me/mobile/android/dao/generator/PMDaoGenerator.java
@@ -6,80 +6,36 @@ import de.greenrobot.daogenerator.Schema;
 
 public class PMDaoGenerator {
 
-    private static final String PACKAGE_NAME = "com.xstd.pirvatephone.dao.model";
-
     private static final int VERSION = 1;
 
     public static void main(String[] args) throws Exception {
-        Schema schema = new Schema(VERSION, PACKAGE_NAME);
-
-        generateChat(schema);
-
-        new DaoGenerator().generateAll(schema, "../../src");
+        generateMQTTDaoModel();
     }
+    
+    
+    private static void generateMQTTDaoModel() {
+        Schema schema = new Schema(VERSION, "com.xstd.pirvatephone.dao.demo");
 
-    private static void generateChat(Schema schema) {
-        Entity note = schema.addEntity("Chat");
+        Entity note = schema.addEntity("PMProtocal");
+        note.addLongProperty("time").primaryKeyAsc();
+        note.addIntProperty("mqttId").unique();
+        note.addStringProperty("content").notNull();
+        note.addIntProperty("count");
+        note.addLongProperty("localId");
+        note.addIntProperty("ownerId");
 
-        note.addIntProperty("direction").notNull();
-        note.addIntProperty("sendStatus");
+        Entity userNote = schema.addEntity("PushUserInfo");
+        userNote.addLongProperty("id").primaryKey();
+        userNote.addLongProperty("lastLoginTime");
+        userNote.addStringProperty("generateId");
+        userNote.addStringProperty("ticket");
+        userNote.addStringProperty("secret_key");
+        userNote.addStringProperty("server");
 
-        note.addLongProperty("ownerId").notNull();
-        note.addLongProperty("conversationId").notNull();
-        note.addLongProperty("localTime").notNull();
-
-        note.addIntProperty("type").notNull();
-        note.addIntProperty("subtype").notNull();
-        note.addLongProperty("txid").primaryKey();
-        note.addLongProperty("time").notNull();
-
-        note.addIntProperty("fromType");
-        note.addLongProperty("fromId").notNull();
-        note.addStringProperty("fromName");
-        note.addStringProperty("fromAvatar");
-
-        note.addIntProperty("toType");
-        note.addLongProperty("toId").notNull();
-        note.addStringProperty("toName");
-        note.addStringProperty("toAvatar");
-
-        note.addIntProperty("relatedType");
-        note.addLongProperty("relatedId");
-        note.addStringProperty("relatedName");
-        note.addStringProperty("relatedAvatar");
-
-        note.addIntProperty("tplId");
-        note.addStringProperty("text");
-        note.addLongProperty("soundId");
-        note.addIntProperty("soundDuration");
-        note.addStringProperty("soundUrl");
-        note.addLongProperty("imgId");
-        note.addStringProperty("imgPrefix");
-        note.addStringProperty("imgOrigin");
-        note.addStringProperty("imgW720");
-        note.addStringProperty("imgW640");
-        note.addIntProperty("imgWidth");
-        note.addIntProperty("imgHeight");
-        note.addStringProperty("imgLocalPath");
-        note.addStringProperty("url");
-        note.addLongProperty("latitude");
-        note.addLongProperty("langitude");
-        note.addLongProperty("altitude");
-        note.addLongProperty("refId");
-        note.addIntProperty("refType");
-        note.addLongProperty("refUserId");
-        note.addStringProperty("refUserName");
-        note.addStringProperty("refUserAvatar");
-        note.addStringProperty("refDesc");
-        note.addLongProperty("refImgId");
-        note.addStringProperty("refImgPrefix");
-        note.addStringProperty("refImgOrigin");
-        note.addStringProperty("refImgW720");
-        note.addStringProperty("refImgW640");
-        note.addIntProperty("refImgWidth");
-        note.addIntProperty("refImgHeight");
-        note.addIntProperty("refPokeCount");
-        note.addLongProperty("refPostTime");
+        try {
+            new DaoGenerator().generateAll(schema, "../../src");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-
 }
