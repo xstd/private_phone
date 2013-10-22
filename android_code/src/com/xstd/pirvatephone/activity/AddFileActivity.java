5390c076f7df7d889a050a4b0432d2693e5a56da
chrain <fly__u@163.com>
2013-10-22 16:54:14 +0800

    修改

--- android_code/src/com/xstd/pirvatephone/activity/AddFileActivity.java
+++ android_code/src/com/xstd/pirvatephone/activity/AddFileActivity.java
@@ -1,12 +1,22 @@
 package com.xstd.pirvatephone.activity;
 
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
 import com.plugin.common.utils.view.ViewMapUtil;
 import com.plugin.common.utils.view.ViewMapping;
-import com.xstd.pirvatephone.R;import com.xstd.privatephone.adapter.AddFileAdapter;
+import com.xstd.pirvatephone.R;
+import com.xstd.pirvatephone.module.MediaModule;
+import com.xstd.privatephone.adapter.AddFileAdapter;
+
+import java.util.ArrayList;
 
 /**
  * Created with IntelliJ IDEA.
@@ -17,6 +27,7 @@ import com.xstd.pirvatephone.R;import com.xstd.privatephone.adapter.AddFileAdapt
  */
 public class AddFileActivity extends BaseActivity implements View.OnClickListener {
 
+    private static final String TAG = "AddFileActivity";
     @ViewMapping(ID = R.id.lv)
     public ListView lv;
 
@@ -26,7 +37,11 @@ public class AddFileActivity extends BaseActivity implements View.OnClickListene
     @ViewMapping(ID = R.id.ll_title_text)
     public TextView ll_title_text;
 
+    AddFileAdapter adapter;
+
     private int privacy_type;
+
+    private ArrayList<MediaModule> datas = new ArrayList<MediaModule>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -39,17 +54,82 @@ public class AddFileActivity extends BaseActivity implements View.OnClickListene
     }
 
     private void initUI() {
-        ViewMapUtil.viewMapping(this,getWindow());
+        ViewMapUtil.viewMapping(this, getWindow());
 
         ll_return_btn.setOnClickListener(this);
-        lv.setAdapter(new AddFileAdapter(getApplicationContext()));
+        adapter = new AddFileAdapter(getApplicationContext());
+        lv.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();    //To change body of overridden methods use File | Settings | File Templates.
+        if (privacy_type == 1) {
+            ll_title_text.setText(R.string.title_select_add_audio);
+        } else if (privacy_type == 2) {
+            ll_title_text.setText(R.string.title_select_add_vedio);
+        }
+        new QueryMediaTask().execute(privacy_type);
     }
 
     @Override
     public void onClick(View v) {
-        //To change body of implemented methods use File | Settings | File Templates.
         if (v == ll_return_btn) {
             finish();
+        }
+    }
+
+    private class QueryMediaTask extends AsyncTask<Integer, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Integer... params) {
+            datas.clear();
+            Cursor cursor = null;
+            if (params[0] == 1) {
+                Log.w(TAG, "audio");
+                cursor = getContentResolver().query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, null, null, null, MediaStore.Audio.Media.DEFAULT_SORT_ORDER);
+                if (cursor != null && cursor.getCount() > 0) {
+                    while (cursor.moveToNext()) {
+                        MediaModule module = new MediaModule();
+                        module.setDisplay_name(cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DISPLAY_NAME)));
+                        module.setSize(cursor.getLong(cursor.getColumnIndex(MediaStore.Audio.Media.SIZE)));
+                        module.setDate_modified(cursor.getLong(cursor.getColumnIndex(MediaStore.Audio.Media.DATE_MODIFIED)));
+                        module.setPath(cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATA)));
+                        datas.add(module);
+                    }
+                }
+            } else if (params[0] == 2) {
+                Log.w(TAG, "vedio");
+                cursor = getContentResolver().query(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, null, null, null, MediaStore.Video.Media.DEFAULT_SORT_ORDER);
+                if (cursor != null && cursor.getCount() > 0) {
+                    Log.w(TAG, "vedio有数据");
+                    while (cursor.moveToNext()) {
+                        MediaModule module = new MediaModule();
+                        module.setDisplay_name(cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.DISPLAY_NAME)));
+                        module.setSize(cursor.getLong(cursor.getColumnIndex(MediaStore.Video.Media.SIZE)));
+                        module.setDate_modified(cursor.getLong(cursor.getColumnIndex(MediaStore.Video.Media.DATE_MODIFIED)));
+                        module.setPath(cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.DATA)));
+
+                        BitmapFactory.Options options = new BitmapFactory.Options();
+                        options.inDither = false;
+                        options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+                        module.setThumb(MediaStore.Video.Thumbnails.getThumbnail(getContentResolver(), cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Video.Media._ID)), MediaStore.Images.Thumbnails.MICRO_KIND, options));
+                        if(module.getThumb()!=null)
+                            Log.w(TAG,module.getThumb().toString());
+                        datas.add(module);
+                    }
+                }
+            }
+            if (cursor != null) {
+                cursor.close();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);    //To change body of overridden methods use File | Settings | File Templates.
+            adapter.changeData(datas);
         }
     }
 }
--- android_code/src/com/xstd/privatephone/adapter/AddFileAdapter.java
+++ android_code/src/com/xstd/privatephone/adapter/AddFileAdapter.java
@@ -4,7 +4,14 @@ import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListAdapter;
+import android.widget.TextView;
+import com.xstd.pirvatephone.R;
+import com.xstd.pirvatephone.module.MediaModule;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -14,26 +21,55 @@ import android.widget.ListAdapter;
  * To change this template use File | Settings | File Templates.
  */
 public class AddFileAdapter extends BaseAdapter {
+
+    private Context mCtx;
+
+    private List<MediaModule> mDatas = new ArrayList<MediaModule>();
+
     public AddFileAdapter(Context context) {
+        mCtx = context;
     }
 
     @Override
     public int getCount() {
-        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+        return mDatas.size();  //To change body of implemented methods use File | Settings | File Templates.
     }
 
     @Override
     public Object getItem(int position) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return mDatas.get(position);  //To change body of implemented methods use File | Settings | File Templates.
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+        return position;  //To change body of implemented methods use File | Settings | File Templates.
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        ViewHolder holder = null;
+        if(convertView == null) {
+            holder = new ViewHolder();
+            convertView = View.inflate(mCtx, R.layout.item_add_file,null);
+            holder.thumb = (ImageView) convertView.findViewById(R.id.thumb);
+//            holder.display_name = (TextView) convertView.findViewById(R.id.display_name);
+//            holder.date = (TextView) convertView.findViewById(R.id.date);
+//            holder.size = (TextView) convertView.findViewById(R.id.size);
+        }
+        return convertView;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    private class ViewHolder {
+        ImageView thumb;
+        TextView display_name;
+        TextView date;
+        TextView size;
+    }
+
+
+    public void changeData(List<MediaModule> datas) {
+        mDatas.clear();
+        mDatas.containsAll(datas);
+        notifyDataSetChanged();
     }
 }
